ae08deacc2d3279251703def39ab73b8a49920ea
Artemii Yanushevskyi <argoniton@gmail.com>
2017-04-04 11:24:33 +0300

    app to add a licenses

---
+++ preparation/licensing/license.py
@@ -0,0 +1,61 @@
+
+##  Turing Machine Visual - a software developed to simulate Turing machine
+##    Copyright (C) 2017 Artemii Yanushevskyi
+##
+##    This program is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU Affero General Public License as
+##    published by the Free Software Foundation, either version 3 of the
+##    License, or (at your option) any later version.
+##
+##    This program is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU Affero General Public License for more details.
+##
+##    You should have received a copy of the GNU Affero General Public License
+##    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+##    You can contact me by email: argoniton@gmail.com
+##
+##  This is BETA 6 of this application.
+
+
+def apply(copyrighttext):
+    import re, os
+    catalog = os.getcwd()+'/subject'
+    for (dirpath, dirnames, filenames) in os.walk(catalog):
+        for filename in filenames:
+            resolution = re.search('\.(.+)$', filename)
+            if resolution.group(1) == 'py':
+                file = open(os.path.join(dirpath, filename), 'r')
+                text = file.read()
+                print(text[2])
+                file.close()
+                textFlat = copyrighttext+'\n\n'+''.join(line for line in text)
+                file = open(os.path.join(dirpath, filename), 'w')
+                print(textFlat, file = file)
+                file.close()
+
+copyrighttext = '''
+##  Turing Machine Visual - a software developed to simulate Turing machine
+##    Copyright (C) 2017 Artemii Yanushevskyi
+##
+##    This program is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU Affero General Public License as
+##    published by the Free Software Foundation, either version 3 of the
+##    License, or (at your option) any later version.
+##
+##    This program is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU Affero General Public License for more details.
+##
+##    You should have received a copy of the GNU Affero General Public License
+##    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+##    You can contact me by email: argoniton@gmail.com
+##
+##  This is BETA 6 of this application.
+'''
+apply(copyrighttext)
+
